1d74b6077936a748d460bcc9c9b75ab9e2e1fe91
johnttaylor <jtaylor@syncroness.com>
2019-04-11 17:33:22 -0600

    test of credentials (again)

--- top/git-credential-helper.sh
+++ top/git-credential-helper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [[ $1 == User* ]]; then
-  echo "$GITHUB_USERNAME"
-else
-  echo "$GITHUB_PASSWORD"
-fi
+#if [[ $1 == User* ]]; then
+#  echo "$GITHUB_USERNAME"
+#else
+#  echo "$GITHUB_PASSWORD"
+#fi
+echo username=$GITHUB_USERNAME
+echo password=$GITHUB_PASSWORD
