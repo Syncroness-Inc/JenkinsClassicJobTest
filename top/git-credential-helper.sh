e455f2e12e2bee39b33025acabb44e4f31d2bf51
johnttaylor <jtaylor@syncroness.com>
2019-04-11 17:23:51 -0600

    working on linux slave build..

--- top/git-credential-helper.sh
+++ top/git-credential-helper.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo "args=$@." >> tmp.txt
 if [[ $1 == User* ]]; then
   echo "$GITHUB_USERNAME"
-  echo "$GITHUB_USERNAME" >> tmp.txt
 else
   echo "$GITHUB_PASSWORD"
-  echo "$GITHUB_PASSWORD" >> tmp.txt
 fi
