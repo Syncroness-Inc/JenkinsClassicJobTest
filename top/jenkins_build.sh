f2ea1fe7176cd5c2c9ba3877dd5889ae69762e76
johnttaylor <jtaylor@syncroness.com>
2019-04-11 14:09:36 -0600

    working on linux slave build..

--- top/jenkins_build.sh
+++ top/jenkins_build.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 # Build script
+# usage: jenkins_build.sh [git <branch>]
+
+# get the CWD
+CWD=`pwd`
 
 # Calculate build number
 MASTER_COUNT=`git rev-list --count origin/master`
 DEVELOP_COUNT=`git rev-list --count origin/develop`
 BLDNUM=$((MASTER_COUNT + DEVELOP_COUNT))
 echo BuildNumber=$BLDNUM > build_number_$BLDNUM.txt
+
+# tag my GIT build
+if [ "$1" == "git" ]; then
+  BRANCHNAME=$2
+  export GIT_ASKPASS="top/git-credential-helper.sh"
+  TAG=$BRANCHNAME-bldnum-$BLDNUM
+  echo "build tag=$TAG" >> build_number_$BLDNUM.txt
+  git log --max-count=1 >> build_number_$BLDNUM.txt
+  git tag $TAG
+  if [ $? -ne 0 ]; then
+    git tag -d $TAG
+    git tag $TAG
+  fi
+  git push origin $TAG
+fi
 
 # Turn on auto-exit-on-error
 set -e
