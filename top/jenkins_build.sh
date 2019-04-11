47d138c32dad4f921cba240cecd2fb3320fb2d64
johnttaylor <jtaylor@syncroness.com>
2019-04-11 14:29:42 -0600

    working on linux slave build..

--- top/jenkins_build.sh
+++ top/jenkins_build.sh
@@ -15,6 +15,7 @@ echo BuildNumber=$BLDNUM > build_number_$BLDNUM.txt
 # tag my GIT build
 if [ "$1" == "git" ]; then
   . ~/.bashrc
+  echo $GITHUB_USERNAME
   BRANCHNAME=$2
   export GIT_ASKPASS="top/git-credential-helper.sh"
   TAG=$BRANCHNAME-bldnum-$BLDNUM
