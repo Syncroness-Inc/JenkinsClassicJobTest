fac2040a5234834ee81eb66515f031502f00375c
johnttaylor <jtaylor@syncroness.com>
2019-04-12 14:59:02 -0600

    move tagging to the end of the build script

--- top/jenkins_build.sh
+++ top/jenkins_build.sh
@@ -12,7 +12,21 @@ DEVELOP_COUNT=`git rev-list --count origin/develop`
 BLDNUM=$((MASTER_COUNT + DEVELOP_COUNT))
 echo BuildNumber=$BLDNUM > build_number_$BLDNUM.txt
 
+
+# Turn on auto-exit-on-error
+set -e
+
+# Run the unittests
+python3 -m pytest --cov src -v --junitxml junit.xml --cov-report xml tests
+
+# Perform the type checking
+python3 -m mypy --config-file top/mypy.ini src
+			    
+# Run the linter
+python3 -m pylint --rcfile=top/pylint.conf src
+
 # tag my GIT build
+set +e
 if [ "$1" == "git" ]; then
   source ~/credentials.sh
   whoami
@@ -30,15 +44,3 @@ if [ "$1" == "git" ]; then
   set -e
   git push origin $TAG
 fi
-
-# Turn on auto-exit-on-error
-set -e
-
-# Run the unittests
-python3 -m pytest --cov src -v --junitxml junit.xml --cov-report xml tests
-
-# Perform the type checking
-python3 -m mypy --config-file top/mypy.ini src
-			    
-# Run the linter
-python3 -m pylint --rcfile=top/pylint.conf src
